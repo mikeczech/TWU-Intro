0aa797127c3f09a20063bfb7b3af2adb413713c2
mczech <mczech@mail.uni-paderborn.de>
2016-07-29 14:39:43 +0200

    made Triangle tests pass

--- src/main/com/thoughtworks/twu/intro/Triangle.java
+++ src/main/com/thoughtworks/twu/intro/Triangle.java
@@ -1,16 +1,33 @@
 package com.thoughtworks.twu.intro;
 
+import java.util.stream.IntStream;
+
 public class Triangle {
 
+    private static void printAsterisk() {
+        System.out.print("*");
+    }
+
+    private static void printNewline() {
+        System.out.print("\n");
+    }
+
     public static void printHorizontalLine(int n) {
-        
+        IntStream.range(0, n).forEach(i -> printAsterisk());
     }
 
     public static void printVerticalLine(int n) {
-        
+        IntStream.range(0, n).forEach(i -> {
+            printAsterisk();
+            printNewline();
+        });
     }
 
     public static void printRightTriangle(int n) {
-        
+        IntStream.range(1, n+1).forEach(i -> {
+            printHorizontalLine(i);
+            if(i < n)
+                printNewline();
+        });
     }
 }
