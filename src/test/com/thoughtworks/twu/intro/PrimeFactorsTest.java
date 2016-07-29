6eeb5d049678c73a4e5e75b45600e3353053c5a2
mczech <mczech@mail.uni-paderborn.de>
2016-07-29 19:27:38 +0200

    added tests for prime factorization exercise

---
+++ src/test/com/thoughtworks/twu/intro/PrimeFactorsTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.twu.intro;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void testFactorizationWithEmptyResult() {
+        List<Integer> factors = PrimeFactors.factorize(0);
+        assertTrue(factors.isEmpty())
+    }
+
+    @Test
+    public void testFactorization() {
+        List<Integer> factors = PrimeFactors.factorize(30);
+        assertEquals(Arrays.asList(2, 3, 5), factors);
+    }
+
+}
