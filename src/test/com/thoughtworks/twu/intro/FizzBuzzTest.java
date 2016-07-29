ca3f7ae0878901e5c4d418d2dec3736c2649f7d5
mczech <mczech@mail.uni-paderborn.de>
2016-07-29 18:06:43 +0200

    added test fo fizzbuzz

---
+++ src/test/com/thoughtworks/twu/intro/FizzBuzzTest.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.twu.intro;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zenscr on 29/07/16.
+ */
+public class FizzBuzzTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testFizzBuzz() throws Exception {
+        FizzBuzz.fizzBuzz(15);
+        assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz", outContent.toString());
+    }
+
+}
